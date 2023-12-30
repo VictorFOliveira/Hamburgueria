0acc8919441f808b97e7b11a7c369d2689340a3d
Victor Facundo <victorfacundo122@hotmail.com>
2023-12-30 18:14:24 -0300

    Adicionado alguns enums.

--- HamburgueriaSana/Enum/Enums.cs
+++ HamburgueriaSana/Enum/Enums.cs
@@ -7,4 +7,23 @@
         Atendente = 2,
         Cozinha = 3
     }
+
+    public enum ReservaMesa
+    {
+        Disponivel = 0,
+        Ocupada = 1
+    }
+
+    public enum StatusPedidos
+    {
+        EmProdução = 0,
+        Concluido = 1
+    }
+
+    public enum ContaMesa
+    {
+        SemPedido = 0,
+        PendentePagamento = 1,
+        Pago = 2
+    }
 }
